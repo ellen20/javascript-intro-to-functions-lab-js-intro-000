bfe409a4a73540d13df2700a1d3afbc305f442d7
jingling jin <jljin0524@gmail.com>
2020-06-18 05:07:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   }else if (string === 'I love you, Grandma.') {
     return "I love you,too."
-    
+
   }
-  
-    
-    
+
+
+
   }
 }
