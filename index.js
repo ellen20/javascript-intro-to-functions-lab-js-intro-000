c54c7902f73a49ced1532715ceab6594c316d82f
jingling jin <jljin0524@gmail.com>
2020-06-18 05:56:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function logWhisper(string) {
 var lowercase = 'hello'
 var uppercase = 'HELLO'
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === lowercase){
+  if (string.toLowerCase() = string){
     return "I can\'t hear you!"
-  }else if (string.toUpperCase() === lowercase) {
+  }else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }else if (string === 'I love you, Grandma.') {
     return 'I love you, too.'
